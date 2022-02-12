e2a0194ec0e92b6d23cd3c27f7c6e4eb9968ecf2
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-02-12 04:20:17 -0300

    feat: Create `react` module to export `useEvent`
    This allows the main module to be used without React.
    
    BREAKING CHANGE: `useEvent` is no longer exported from the root.

---
+++ build.js
@@ -0,0 +1,41 @@
+const { build } = require("esbuild");
+
+function exit() {
+  process.exit(1);
+}
+
+build({
+  entryPoints: ["./src/index.ts"],
+  outfile: "./dist/index.js",
+  logLevel: "info",
+  format: "cjs",
+  bundle: true,
+}).catch(exit);
+
+// build({
+//   entryPoints: ["./src/index.ts"],
+//   outfile: "./dist/index.browser.js",
+//   logLevel: "info",
+//   format: "iife",
+//   globalName: "events",
+//   platform: "browser",
+//   minify: true,
+// }).catch(exit);
+
+// build({
+//   entryPoints: ["./src/index.ts"],
+//   outfile: "./dist/index.mjs",
+//   logLevel: "info",
+//   format: "esm",
+//   platform: "browser",
+//   minify: true,
+// }).catch(exit);
+
+build({
+  entryPoints: ["./src/react/index.ts"],
+  outfile: "./dist/react/index.js",
+  logLevel: "info",
+  format: "cjs",
+  bundle: true,
+  external: ["react", "."],
+}).catch(exit);
