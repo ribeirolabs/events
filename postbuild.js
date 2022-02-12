5fdbb72c5cdea85c24bdaa2e9087d16dc66d54cd
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-02-12 04:59:52 -0300

    fix(build): Transform dist/package.json to fix npm publish

--- postbuild.js
+++ postbuild.js
@@ -1,9 +1,16 @@
 const fs = require("fs");
 
-const packageJson = fs.readFileSync("./package.json", "utf-8");
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
+
+delete packageJson.scripts;
+delete packageJson.files;
+delete packageJson.devDependencies;
 
 fs.writeFileSync(
   "./dist/package.json",
-  packageJson.replace(/dist\//g, ""),
+  JSON.stringify(packageJson).replace(/dist\//g, ""),
   "utf-8"
 );
+
+fs.copyFileSync("./README.md", "./dist/README.md");
+fs.copyFileSync("./LICENSE", "./dist/LICENSE");
